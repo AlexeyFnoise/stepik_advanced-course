fcb7ebffef6a3373a5af41bf71e9de1beb3d60e3
AlexeyFnoise <alexeyfnoise@yandex.ru>
2025-05-12 14:00:31 +0500

    2.1 Часть 1 Задача Иосифа Флавия

--- stepik_advanced.py
+++ stepik_advanced.py
@@ -1,3 +1,11 @@
 a, b = int(input()), int(input())
 print(a + b, a - b, a * b, a / b, a // b, a % b, (a ** 10 + b ** 10) ** 0.5, sep='\n')
 
+#Задача Иосифа Флавия 🌶️🌶️
+n, k = int(input()), int(input())
+s = [i for i in range(1, n + 1)]
+while len(s) > 1:
+    for q in range(0, k - 1):
+        s.append(s[q])
+    del s[:k]
+print(*s)
