476346953ec8e2180d8f0c7905fd8f3917ee270c
AlexeyFnoise <alexeyfnoise@yandex.ru>
2025-05-30 13:36:51 +0500

    6.2 Основы работы с кортежами. Часть 1

--- stepik_advanced.py
+++ stepik_advanced.py
@@ -419,3 +419,7 @@ for _ in range(x - 1):
     ls1 = ls3
 for i in ls1:
     print(*i)
+
+tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+new_tuples = [(i[:-1]) + (100,) for i in tuples]
+print(new_tuples)
