600a5684485332efe432a8378c595ffdfadfe4db
AlexeyFnoise <alexeyfnoise@yandex.ru>
2025-06-24 16:43:32 +0500

    10.2 Основы работы со словарями

--- stepik_advanced.py
+++ stepik_advanced.py
@@ -513,6 +513,7 @@ print(*sorted(set_new - set1 - set2 - set3))
 words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
 newset = {i[0].lower() for i in words}
 print(*sorted(newset))
+l
 
 # Книги на прочтение 📚
 m, n = int(input()), int(input())
@@ -526,3 +527,24 @@ res = {input() for _ in range(int(input()))}
 for _ in range(n - 1):
     res &= {input() for _ in range(int(input()))}
 print(*sorted(res), sep= '\n')
+
+users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
+         {'name': 'Helga', 'phone': '555-1618'},
+         {'name': 'Olivia', 'phone': '449-3141', 'email': ''},
+         {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@gmail.net'},
+         {'name': 'Ruslan', 'phone': '422-145-9098', 'email': 'rus-lan.cha@yandex.ru'},
+         {'name': 'John', 'phone': '233-421-32', 'email': ''},
+         {'name': 'Lara', 'phone': '+7998-676-2532', 'email': 'g.lara89@gmail.com'},
+         {'name': 'Alina', 'phone': '+7948-799-2434'},
+         {'name': 'Robert', 'phone': '420-2011', 'email': ''},
+         {'name': 'Riyad', 'phone': '128-8890-128', 'email': 'r.mahrez@mail.net'},
+         {'name': 'Khabib', 'phone': '+7995-600-9080', 'email': 'kh.nurmag@gmail.com'},
+         {'name': 'Olga', 'phone': '6449-314-1213', 'email': ''},
+         {'name': 'Roman', 'phone': '+7459-145-8059'},
+         {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
+         {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
+         {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+newlis = []
+for el in users:
+    if 'email' not in el or el['email'] == '' : newlis.append(el['name'])
+print(*sorted(newlis))
