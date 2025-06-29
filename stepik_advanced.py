7beb4126c96479614a492c418fca49478491cb0b
Fnoise <alexeyfnoise@yandex.ru>
2025-06-29 21:46:43 +0500

    10.5 Вложенные словари и генераторы словарей Конец

--- stepik_advanced.py
+++ stepik_advanced.py
@@ -751,11 +751,61 @@ for _ in range(int(input())):
 for i in s: print(my_dict[s.count(i)], end='')
 
 #Генератор словарей
-numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
+numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95,
+           1, 36, -38, -19, 1, 6, 87]
 result = {i: numbers[i] ** 2 for i in range(len(numbers))}
 print(result)
 
 #Генератор словарей 5
 s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
 result = {int(el.split(':')[0]): el.split(':')[1] for el in s.split()}
+print(result)
+
+#Генератор словарей 6
+numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21,
+           35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+result = {i: sorted([k for k in range(1, i + 1) if i % k == 0])for i in numbers}
+print(result)
+
+#Генератор словарей 7
+words = ['hello', 'bye', 'yes', 'no', 'python',
+         'apple', 'maybe', 'stepik', 'beegeek']
+result = {i: [ord(symb) for symb in i]for i in words}
+print(result)
+
+#Генератор словарей 8
+letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D',
+           4: 'E', 5: 'F', 6: 'G', 7: 'H',
+           8: 'I', 9: 'J', 10: 'K', 11: 'L',
+           12: 'M', 13: 'N', 14: 'O', 15: 'P',
+           16: 'Q', 17: 'R', 18: 'S', 19: 'T',
+           20: 'U', 21: 'V', 22: 'W', 23: 'X',
+           24: 'Y', 26: 'Z'}
+remove_keys = [1, 5, 7, 12, 17, 19, 21, 24]
+result = {key: value for key, value in letters.items() if key not in remove_keys}
+
+#Генератор словарей 9
+students = {'Timur': (170, 75), 'Ruslan': (180, 105),
+            'Soltan': (192, 68), 'Roman': (175, 70),
+            'Madlen': (160, 50), 'Stefani': (165, 70),
+            'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67),
+            'Scott': (168, 78), 'John': (186, 79), 'Alex': (195, 120),
+            'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80),
+            'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80),
+            'Mary': (175, 69), 'Jane': (190, 80)}
+result = {k: v for k, v in students.items() if v[0] > 167 and v[1] < 75}
+
+#Генератор словарей 10
+tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12),
+          (13, 14, 15), (16, 17, 18), (19, 20, 21),
+          (22, 23, 24), (25, 26, 27), (28, 29, 30),
+          (31, 32, 33), (34, 35, 36)]
+result = {i[0]: i[1:] for i in tuples}
+print(result)
+
+#Генератор словарей 11
+student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+result = [{i: {k: q}}for i, k, q in zip(student_ids, student_names, student_grades)]
 print(result)
