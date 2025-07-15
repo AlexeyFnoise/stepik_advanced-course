bfa6b0dfc0eb67b546f7810a3f82acc74359f0e5
alexey <alexeyfnoise@yandex.ru>
2025-07-15 20:29:15 +0500

    12.2 Модуль random. Часть 2

--- stepik_advanced.py
+++ stepik_advanced.py
@@ -906,3 +906,24 @@ print(s)
 # Лотерейный билет
 # import random
 print(*sorted([random.randint(1, 49) for _ in range(7)]))
+
+my_dict = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'}
+ls = [int(i) for i in input()]
+ls_new = [my_dict[i] for i in ls]
+print(*ls_new)
+
+# Напишите функцию generate_ip()
+# import random
+def generate_ip():
+    return f'{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}'
+generate_ip()
